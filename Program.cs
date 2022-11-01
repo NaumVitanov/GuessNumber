425469f401038cdcc7e7a9fbebeff2b96f337771
Naum <vitanovnaum@gmail.com>
2022-11-01 11:11:45 +0200

    Added new functionalities
    Added option to choose difficulty .Added progressing levels and a option to play again.

--- Program.cs
+++ Program.cs
@@ -1,36 +1,131 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GuessNumber
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.Write("Guess a number (1-100): ");            
-            Random random = new Random();
-            int pcNum = random.Next(1, 101);
-            int userNum = 0;
-            
-            while (userNum != pcNum)
+            Console.WriteLine("Please select difficuly.You can choose between : [E]asy , [M]edium , [H]ard");
+            string playerDifficulty;
+            List<string> possibleDifficulty = new List<string> { "easy", "medium", "hard", "e", "m", "h" };
+            int tries = 0;
+            int currLevel = 1;
+            while (true)
             {
-                if(!int.TryParse(Console.ReadLine(), out userNum))
+                playerDifficulty = Console.ReadLine().ToLower();
+                bool flag = false;
+
+                if(!possibleDifficulty.Contains(playerDifficulty))
                 {
-                    Console.WriteLine("Invalid Input!");
-                    Console.Write("Guess a number (1-100): ");
+                    Console.WriteLine("Please input either the first letter or the whole word of the selected difficulty!");
                     continue;
                 }
-                if (userNum > pcNum)
+
+                for(int i = 0; i < possibleDifficulty.Count; i++)
                 {
-                    Console.WriteLine("Too High");
-                    Console.Write("Guess a number (1-100): ");
+                    if(possibleDifficulty[i] == playerDifficulty)
+                    {                        
+                        if (playerDifficulty == "e" || playerDifficulty == "easy")
+                        {
+                            tries = 10;
+                            Console.WriteLine("You have chosen easy difficulty.");
+                            Console.WriteLine("You have 10 tries to guess.");
+                        }
+                        else if (playerDifficulty == "m" || playerDifficulty == "medium")
+                        {
+                            tries = 6;
+                            Console.WriteLine("You have chosen medium difficulty.");
+                            Console.WriteLine("You have 6 tries to guess.");
+                        }
+                        else if (playerDifficulty == "h" || playerDifficulty == "hard")
+                        {
+                            tries = 3;
+                            Console.WriteLine("You have chosen hard difficulty.");
+                            Console.WriteLine("You have 3 tries to guess.");
+                        }
+                        flag = true;
+                        break;
+                    }
                 }
-                else if (userNum < pcNum)
+
+                if(flag)
                 {
-                    Console.WriteLine("Too Low");
-                    Console.Write("Guess a number (1-100): ");
+                    break;
+                }
+            }
+
+            int upperNumber = 101;
+
+            for (int i = 0; i < 1; i++)
+            {       
+                if(currLevel == 6)
+                {
+                    Console.WriteLine("You are very good at guessing.You WIN!");
+                    return;
+                }
+                Random random = new Random();
+                int pcNum = random.Next(1, upperNumber);
+
+                for ( int j = 0; j < tries; j++)
+                {
+                    Console.Write($"Guess a number (1-{upperNumber - 1}): ");
+                    if (!int.TryParse(Console.ReadLine(), out int userNum))
+                    {
+                        j--;
+                        Console.WriteLine("Invalid Input!");                        
+                        continue;
+                    }
+                    if (userNum > pcNum)
+                    {
+                        if(userNum > upperNumber - 1)
+                        {
+                            j--;
+                            Console.WriteLine("Please choose a valid number!");
+                            continue;
+                        }
+                        Console.WriteLine("Too High");                        
+                    }
+                    else if (userNum < pcNum)
+                    {
+                        if (userNum < 1)
+                        {
+                            j--;
+                            Console.WriteLine("Please choose a valid number!");
+                            continue;
+                        }
+                        Console.WriteLine("Too Low");                        
+                    }
+                    else
+                    {                        
+                        upperNumber += 100;                       
+                        currLevel++;
+                        i--;
+                        Console.WriteLine("You guessed it!");
+                        if (upperNumber > 600)
+                        {                            
+                            upperNumber = 1001;
+                            Console.WriteLine("You are playing the last level 6!");
+                            break;
+                        }
+                        Console.WriteLine($"You have gone up one level to level {currLevel}!");
+                        break;
+                    }
                 }                
             }
-            Console.WriteLine("You guessed it!");
+            Console.WriteLine("You loose!");
+            Console.WriteLine("Do you want to play again?");
+            string playAgain = Console.ReadLine().ToLower();
+
+            if(playAgain == "yes")
+            {
+                Main();
+            }
+            else if (playAgain == "no")
+            {
+                Console.WriteLine($"You ended your strike at level {currLevel}");
+            }
         }
     }
 }
